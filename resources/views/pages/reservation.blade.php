3a69e3918aec667e860cdd170d68d5bcbdb26fb2
dfbtiv <dfebriyanti204@gmail.com>
2026-01-25 22:48:50 +0700

    update reservasi page

--- resources/views/pages/reservation.blade.php
+++ resources/views/pages/reservation.blade.php
@@ -4,8 +4,44 @@
 
 @section('content')
 <section id="reservation">
-    <h1> Make a reservation</h1>
-    <p> Reservation Page</p>
-
+    <div class="reservation-text">
+        <h6> Reservation </h6>
+        <h2> Book a Table </h2>
+        <p> Please complete the form to book your table. <br> Let us know your details, preferred date and time, and the number of guests. </p>
+    </div>
+    <!-- form -->
+    <form class="reservation-container">
+        <!-- row 1 -->
+        <div class="form-row">
+            <div class="form-field">
+                <label class="form-label">Name</label>
+                <input type="text" class="form-box" placeholder="John Doe">
+            </div>
+            <div class="form-field">
+                <label class="form-label">Email Address</label>
+                <input type="email" class="form-box" placeholder="example@gmail.com">
+            </div>
+            <div class="form-field">
+                <label class="form-label">Phone Number</label>
+                <input type="tel" class="form-box" placeholder="62123455689">
+            </div>
+        </div>
+        <!-- row 2 -->
+            <div class="form-row">
+            <div class="form-field">
+                <label class="form-label">Date & Time</label>
+                <input type="datetime-local" class="form-box">
+            </div>
+            <div class="form-field">
+                <label class="form-label">Person</label>
+                <input type="number" class="form-box">
+            </div>
+        </div>
+        <!-- message -->
+        <div class="form-field full width">
+            <label class="form-label">Message</label>
+            <textarea class="form-box" rows="3"></textarea>
+        </div>
+    </form>
 </section>
 @endsection
