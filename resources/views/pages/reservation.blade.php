47626acd14acd0d6934c15397c5e5192a74fecdf
dfbtiv <dfebriyanti204@gmail.com>
2026-01-27 00:14:15 +0700

    update reserv page

--- resources/views/pages/reservation.blade.php
+++ resources/views/pages/reservation.blade.php
@@ -3,31 +3,34 @@
 @section('title', 'Reservation')
 
 @section('content')
-<section id="reservation">
-    <div class="reservation-text">
-        <h6> Reservation </h6>
-        <h2> Book a Table </h2>
-        <p> Please complete the form to book your table. <br> Let us know your details, preferred date and time, and the number of guests. </p>
-    </div>
-    <!-- form -->
-    <form class="reservation-container">
-        <!-- row 1 -->
-        <div class="form-row">
-            <div class="form-field">
-                <label class="form-label">Name</label>
-                <input type="text" class="form-box" placeholder="John Doe">
-            </div>
-            <div class="form-field">
-                <label class="form-label">Email Address</label>
-                <input type="email" class="form-box" placeholder="example@gmail.com">
-            </div>
-            <div class="form-field">
-                <label class="form-label">Phone Number</label>
-                <input type="tel" class="form-box" placeholder="62123455689">
-            </div>
+    @include('partials.navbar')
+    @include('partials.heroRsvp')
+    <section id="reservation">
+        <div class="reservation-text">
+            <h6> Reservation </h6>
+            <h2> Book a Table </h2>
+            <p> Please complete the form to book your table. <br> Let us know your details, preferred date and time, and the number of guests. </p>
         </div>
+        <!-- form -->
+        <div class="reservation-card">
+            <form class="reservation-container">
+                <!-- row 1 -->
+                <div class="form-row">
+                    <div class="form-field">
+                    <label class="form-label">Name</label>
+                    <input type="text" class="form-box" placeholder="John Doe">
+                </div>
+                <div class="form-field">
+                    <label class="form-label">Email Address</label>
+                    <input type="email" class="form-box" placeholder="example@gmail.com">
+                </div>
+                <div class="form-field">
+                    <label class="form-label">Phone Number</label>
+                    <input type="tel" class="form-box" placeholder="62123455689">
+                </div>
+            </div>
         <!-- row 2 -->
-            <div class="form-row">
+        <div class="form-row">
             <div class="form-field">
                 <label class="form-label">Date & Time</label>
                 <input type="datetime-local" class="form-box">
@@ -42,6 +45,13 @@
             <label class="form-label">Message</label>
             <textarea class="form-box" rows="3"></textarea>
         </div>
+        <!-- button submit -->
+         <div class="form-actions">
+             <button type="submit"> Submit </button>
+        </div>
     </form>
+</div>
 </section>
+    @include('partials.contact')
+    @include('partials.footer')
 @endsection
---
+++ resources/views/partials/heroRsvp.php
@@ -0,0 +1,7 @@
+    <!-- section hero -->
+    <section id="hero-rsvp">
+        <div class="hero-rsvp-text">
+            <h1> Reservation </h1>
+            <p> Home > Reservation </p>
+        </div>
+    </section>
